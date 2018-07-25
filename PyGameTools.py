20ef0cf115a669aa25d05a5e3679686356547dab
Joe Gibson <joe.gibby@hotmail.co.uk>
2018-07-25 16:19:19 +0100

    reworked text box (already)
    new function is slightly faster, and includes italic and bold integration
    
    doesn't yet wrap single words that are too long for a line

--- PyGameTools.py
+++ PyGameTools.py
@@ -1,7 +1,7 @@
 import pygame
 
 
-def text_box(text, colour=(0, 0, 0), size=20, screen=None, coords=None, max_len=2000, gap="auto", font="Arial", rotate=0, rotate_mode="centre", align=("left", "top"), background=None):
+def other_text_box(text, colour=(0, 0, 0), size=20, screen=None, coords=None, max_len=None, gap="auto", font="Arial", italic_font="auto", bold_font="auto", rotate=0, rotate_mode="centre", align=("left", "top"), background=None):
     """Creates a textbox.
     text - str - the text to be written
     colour - list (int, int, int) - the colour of the text
@@ -28,88 +28,117 @@ def text_box(text, colour=(0, 0, 0), size=20, screen=None, coords=None, max_len=
     font_obj = pygame.font.SysFont(font, size)
     text_surface = pygame.Surface((0, 0))
     text_surface.fill(background)
-    wordinc = None
-    text = text.split("\n")
+    text = [t.split() for t in text.split("\n")]
     width = 0
     height = 0
-    
-    for line in text:
-        if line == "":
-            height += size + gap
-            temp_surface = pygame.Surface((width, height))
-            temp_surface.fill(background)
-            temp_surface.blit(text_surface, (0, 0))
-            text_surface = temp_surface
-        inc = len(line.split())
-        while inc > 0:
-            line_surface = font_obj.render(line, False, colour)
-            a = line_surface.get_width()
-            if a > max_len:
-                inc = round(len(line.split()) * (max_len / a))
-                line_surface = font_obj.render(" ".join(line.split()[:inc]), False, colour)
-                a = line_surface.get_width()
-                if a > max_len:
-                    if inc == 1:
-                        word = line.split()[0]
-                        wordinc = round(len(word) * (max_len / a))
-                        line_surface = font_obj.render(word[:wordinc], False, colour)
-                        a = line_surface.get_width()
-                        if a > max_len:
-                            while a > max_len:
-                                wordinc -= 1
-                                line_surface = font_obj.render(word[:wordinc], False, colour)
-                                a = line_surface.get_width()
-                        elif a < max_len:
-                            while a < max_len:
-                                wordinc += 1
-                                line_surface = font_obj.render(word[:wordinc], False, colour)
-                                a = line_surface.get_width()
-                    else:
-                        while a > max_len:
-                            inc -= 1
-                            line_surface = font_obj.render(" ".join(line.split()[:inc]), False, colour)
-                            a = line_surface.get_width()
-                elif a < max_len:
-                    while a < max_len:
-                        inc += 1
-                        line_surface = font_obj.render(" ".join(line.split()[:inc]), False, colour)
-                        a = line_surface.get_width()
-                    inc -= 1
-                    line_surface = font_obj.render(" ".join(line.split()[:inc]), False, colour)
-                    a = line_surface.get_width()
-            height += size + gap
-            if a > width:
-                width = a
-            if inc == 1 and wordinc is not None:
-                line = " ".join([word[wordinc:]] + line.split()[1:])
-                wordinc = None
-            else:
-                line = " ".join(line.split()[inc:])
+    space = font_obj.size(" ")[0]
 
-            temp_surface = pygame.Surface((width, height))
-            temp_surface.fill(background)
-            if align[0] == "left":
-                temp_surface.blit(text_surface, (0, 0))
-                temp_surface.blit(line_surface, (0, text_surface.get_height()))
-            elif align[0] == "centre":
-                temp_surface.blit(text_surface, ((width - text_surface.get_width()) // 2, 0))
-                temp_surface.blit(line_surface, ((width - line_surface.get_width()) // 2, text_surface.get_height()))
+    for line in text:
+        line_surface = pygame.Surface((0, 0))
+        line_surface.fill(background)
+        for word in line:
+            word_surface = pygame.Surface((0, 0))
+            word_surface.fill(background)
+            asterisks = 0
+            for letter in word:
+                if letter == "*":
+                    asterisks += 1
+                    if asterisks == 3:
+                        font_obj.set_italic(not font_obj.get_italic())
+                        font_obj.set_bold(not font_obj.get_bold())
+                        asterisks = 0
+                else:
+                    if asterisks == 1:
+                        font_obj.set_italic(not font_obj.get_italic())
+                    elif asterisks == 2:
+                        font_obj.set_bold(not font_obj.get_bold())
+                    asterisks = 0
+                    letter_surface = font_obj.render(letter, False, colour)
+                    if font_obj.get_italic():
+                        x = 0
+                        y = -1
+                        col = background
+                        while col != tuple(colour):
+                            y += 1
+                            if y == letter_surface.get_height():
+                                y = 0
+                                x += 1
+                            col = letter_surface.get_at((x, y))
+                        left = x
+                        x = letter_surface.get_width() - 1
+                        y = -1
+                        col = background
+                        while col != tuple(colour):
+                            y += 1
+                            if y == letter_surface.get_height():
+                                x -= 1
+                                y = 0
+                            col = letter_surface.get_at((x, y))
+                        right = x + 1.
+                        temp_surface = pygame.Surface((right - left, letter_surface.get_height()))
+                        temp_surface.fill(background)
+                        temp_surface.blit(letter_surface, (0, 0), area=(left, 0, right, letter_surface.get_height()))
+                        letter_surface = temp_surface
+                    temp_surface = pygame.Surface((letter_surface.get_width() + word_surface.get_width(), max(letter_surface.get_height(), word_surface.get_height())))
+                    temp_surface.fill(background)
+                    temp_surface.blit(word_surface, (0, 0))
+                    temp_surface.blit(letter_surface, (word_surface.get_width(), 0))
+                    word_surface = temp_surface
+            if max_len is None:
+                temp_surface = pygame.Surface((line_surface.get_width() + word_surface.get_width() + space, max(line_surface.get_height(), word_surface.get_height())))
+                temp_surface.fill(background)
+                temp_surface.blit(line_surface, (0, 0))
+                temp_surface.blit(word_surface, (line_surface.get_width() + space, 0))
+                line_surface = temp_surface
             else:
-                temp_surface.blit(text_surface, (width - text_surface.get_width(), 0))
-                temp_surface.blit(line_surface, (width - line_surface.get_width(), text_surface.get_height()))
-            text_surface = temp_surface
-            
-            inc = len(line.split())
-    width += 2 * gap
-    height += 2 * gap
-    temp_surface = pygame.Surface((width, height))
-    temp_surface.fill(background)
-    temp_surface.blit(text_surface, (gap, gap))
-    text_surface = temp_surface
-    if transparent:
-        text_surface.set_colorkey(background)
-    plain_text_surface = text_surface
-    
+                if line_surface.get_width() == 0:
+                    line_surface = word_surface
+                else:
+                    if line_surface.get_width() + word_surface.get_width() + space <= max_len:
+                        temp_surface = pygame.Surface((line_surface.get_width() + word_surface.get_width() + space, max(line_surface.get_height(), word_surface.get_height())))
+                        temp_surface.fill(background)
+                        temp_surface.blit(line_surface, (0, 0))
+                        temp_surface.blit(word_surface, (line_surface.get_width() + space, 0))
+                        line_surface = temp_surface
+                    else:
+                        width = max(width, line_surface.get_width())
+                        height += size + gap
+                        temp_surface = pygame.Surface((width, height))
+                        temp_surface.fill(background)
+                        if align[0] == "left":
+                            temp_surface.blit(text_surface, (0, 0))
+                            temp_surface.blit(line_surface, (0, text_surface.get_height()))
+                        elif align[0] == "centre":
+                            temp_surface.blit(text_surface,
+                                              ((width - text_surface.get_width()) // 2, 0))
+                            temp_surface.blit(line_surface, (
+                            (width - line_surface.get_width()) // 2, text_surface.get_height()))
+                        else:
+                            temp_surface.blit(text_surface, (width - text_surface.get_width(), 0))
+                            temp_surface.blit(line_surface, (
+                            width - line_surface.get_width(), text_surface.get_height()))
+                        text_surface = temp_surface
+                        line_surface = word_surface
+            if asterisks == 1:
+                font_obj.set_italic(not font_obj.get_italic())
+            elif asterisks == 2:
+                font_obj.set_bold(not font_obj.get_bold())
+        width = max(width, line_surface.get_width())
+        height += size + gap
+        temp_surface = pygame.Surface((width, height))
+        temp_surface.fill(background)
+        if align[0] == "left":
+            temp_surface.blit(text_surface, (0, 0))
+            temp_surface.blit(line_surface, (0, text_surface.get_height()))
+        elif align[0] == "centre":
+            temp_surface.blit(text_surface, ((width - text_surface.get_width()) // 2, 0))
+            temp_surface.blit(line_surface,
+                              ((width - line_surface.get_width()) // 2, text_surface.get_height()))
+        else:
+            temp_surface.blit(text_surface, (width - text_surface.get_width(), 0))
+            temp_surface.blit(line_surface,
+                              (width - line_surface.get_width(), text_surface.get_height()))
+        text_surface = temp_surface
     if (screen, coords) != (None, None):
         if rotate_mode == "absolute":
             text_surface = pygame.transform.rotate(text_surface, rotate)
@@ -120,17 +149,15 @@ def text_box(text, colour=(0, 0, 0), size=20, screen=None, coords=None, max_len=
             x -= text_surface.get_width() // 2
         elif align[0] == "right":
             x -= text_surface.get_width()
-        
         if align[1] == "centre":
             y -= text_surface.get_height() // 2
         elif align[1] == "bottom":
             y -= text_surface.get_height()
-        
         if rotate_mode == "centre":
             text_surface = pygame.transform.rotate(text_surface, rotate)
             c, d = text_surface.get_size()
             x -= (c - a) // 2
             y -= (d - b) // 2
-        
+        if transparent:
+            text_surface.set_colorkey(background)
         screen.blit(text_surface, (x, y))
-    return plain_text_surface
--- tests.py
+++ tests.py
@@ -4,9 +4,10 @@ import PyGameTools
 
 ##-- TEXT BOX --##
 
-h = 750
-w = 1300
+h = 800
+w = 1400
 m = 400
+cushion = 0
 pygame.init()
 Screen = pygame.display.set_mode((w, h))
 
@@ -15,22 +16,26 @@ This is a new line.
 
 That was a blank line.
 This is a longer line to make sure that everything is wrapping correctly.
+Therearenospacesinthislinetocheckthatlongwordsdon'tcauseaproblem
+*italic* **bold *both***
 This is the last line."""
-
 r, g, b, rot = 0, 0, 0, 0
 while True:
     pygame.display.update()
     Screen.fill((255, 255, 255))
-    pygame.draw.line(Screen, (0, 0, 0), (m, 0), (m, h))
+    for x, xal in ((0, "left"), (w // 2, "centre"), (w, "right")):
+        for y, yal in ((0, "top"), (h // 2, "centre"), (h, "bottom")):
+            PyGameTools.other_text_box(t, (r, g, b), 18, screen=Screen, coords=(x, y), align=(xal, yal), max_len=m, rotate=rot, background=(255 - r, 255 - g, 255 - b))
+    pygame.draw.line(Screen, (0, 0, 0), (cushion, 0), (cushion, h))
+    pygame.draw.line(Screen, (0, 0, 0), (m + cushion, 0), (m + cushion, h))
     pygame.draw.line(Screen, (0, 0, 0), ((w - m) // 2, 0), ((w - m) // 2, h))
     pygame.draw.line(Screen, (0, 0, 0), (w // 2, 0), (w // 2, h))
     pygame.draw.line(Screen, (0, 0, 0), ((w + m) // 2, 0), ((w + m) // 2, h))
-    pygame.draw.line(Screen, (0, 0, 0), (w - m, 0), (w - m, h))
+    pygame.draw.line(Screen, (0, 0, 0), (w - m - cushion, 0), (w - m - cushion, h))
+    pygame.draw.line(Screen, (0, 0, 0), (w - cushion, 0), (w - cushion, h))
     pygame.draw.line(Screen, (0, 0, 0), (0, h // 2), (w, h // 2))
-    for x, xal in ((0, "left"), (w // 2, "centre"), (w, "right")):
-        for y, yal in ((0, "top"), (h // 2, "centre"), (h, "bottom")):
-            PyGameTools.text_box(t, (r, g, b), 20, screen=Screen, coords=(x, y), align=(xal, yal), max_len=m, rotate=rot, background=(255 - r, 255 - g, 255 - b))
-    PyGameTools.text_box(t, (r, g, b), 20, screen=Screen, coords=(0, 0), max_len=m, rotate=rot, background=(255 - r, 255 - g, 255 - b))
+    pygame.draw.line(Screen, (0, 0, 0), (0, cushion), (w, cushion))
+    pygame.draw.line(Screen, (0, 0, 0), (0, h - cushion), (w, h - cushion))
     #for _ in range(1000000):
     #    pass
     if r < 255:
